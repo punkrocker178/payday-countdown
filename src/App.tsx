94c980fb6b9c78bfdfe9401b569236d1710b0c90
punkrocker178 <ngochieu.le1708@gmail.com>
2025-01-30 11:48:38 +0700

    Refactor counter component Added paydate form control Install sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import { createSignal, type Component } from 'solid-js';
+import { createMemo, createSignal, type Component } from 'solid-js';
 import { Counter } from './components/Counter';
+import './App.scss';
 import { DateTime } from 'luxon';
+import { createForm, getValue } from '@modular-forms/solid';
+import { effect } from 'solid-js/web';
+
+type PayDayForm = {
+  payDate: number;
+}
 
 const App: Component = () => {
+  const [dateForm, { Form, Field }] = createForm<PayDayForm>();
   const [payday, setPayday] = createSignal<DateTime<boolean>>(DateTime.fromObject({ day: 5 }).plus({ months: 1 }));
-  const [paydayDisplay, setPaydayDisplay] = createSignal<string>(payday().toFormat('dd/MM/yyyy'));
+  const paydayDisplay = createMemo<string>(() => {
+    return payday().toFormat('dd/MM/yyyy');
+  })
+
+  effect(() => {
+    const payDate = getValue(dateForm, 'payDate');
+    const today = DateTime.now().day;
+
+    if (payDate && today > payDate) {
+      setPayday(DateTime.fromObject({ day: payDate }).plus({ months: 1 }))
+    } else if (payDate && today < payDate) {
+      setPayday(DateTime.fromObject({ day: payDate }))
+    }
+  });
 
   return (
     <div class="container mx-auto">
+      <div class="mb-12">
+        <label for="payDate" class="font-semibold">When is your payday</label>
+        <Form>
+          <Field name="payDate" type="number">
+            {(field, props) => <input class="block form" id="payDate" type="number" max="31" min="1" {...props}></input>}
+          </Field>
+        </Form>
+      </div>
       <div class="text-5xl">
         <h2 class="font-bold">Next Payday</h2>
         <p class="font-semibold">{paydayDisplay()}</p>
@@ -16,11 +45,6 @@ const App: Component = () => {
       <div class="w-1/3 flex flex-col mx-auto">
         <img class="w-full mb-4" alt="meme-1" src="/img/meme1.jpg"></img>
         <img class="w-full" alt="meme-2" src="/img/meme2.jpg"></img>
-      </div>
-
-      <div>
-        <label for="salaryDate">When is your payday</label>
-        <input class="block" id="salaryDate" type="date"></input>
       </div>
     </div>
   );
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,59 +1,59 @@
 import { DateTime } from "luxon";
-import { Component, createEffect, createSignal, onCleanup } from "solid-js";
+import { Component, createMemo, createSignal, onCleanup } from "solid-js";
 import { CounterDates } from "../models/counter-date";
 
 export const Counter: Component<CounterDates> = (props) => {
-    const [currentDate, setCurrentDate] = createSignal<DateTime>();
-    const [countdown, setCountdown] = createSignal<string>();
-    const [displayDayCountdown, setDisplayDayCountdown] = createSignal<string>();
+  const [currentDate, setCurrentDate] = createSignal<DateTime>();
+  const payDate = () => props.paydayDate;
 
-    const formatTimeNumber = (timeUnit: number) => ~~timeUnit < 10 ? `0${~~timeUnit}` : `${~~timeUnit}`;
+  const formatTimeNumber = (timeUnit: number) => ~~timeUnit < 10 ? `0${~~timeUnit}` : `${~~timeUnit}`;
+  const formatTime = (hour: number, minute: number, second: number) => {
+    let displayHour, displayMinute, displaySecond: string;
+    displayHour = formatTimeNumber(hour);
+    displayMinute = formatTimeNumber(minute);
+    displaySecond = formatTimeNumber(second);
+    return `${displayHour}:${displayMinute}:${displaySecond}`;
+  }
 
-    const formatTime = (hour: number, minute: number, second: number) => {
-        let displayHour, displayMinute, displaySecond: string;
-        displayHour = formatTimeNumber(hour);
-        displayMinute = formatTimeNumber(minute);
-        displaySecond = formatTimeNumber(second);
-        return `${displayHour}:${displayMinute}:${displaySecond}`;
+  const displayHoursCountdown = createMemo<string>(() => {
+    const durationDays = currentDate()?.diff(payDate(), 'days').days;
+    if (durationDays) {
+      hourCountdown = (Math.abs(durationDays) % 1) * 24;
+      minuteCountdown = (hourCountdown % 1) * 60;
+      secondCountdown = (minuteCountdown % 1) * 60;
+
+      return formatTime(hourCountdown, minuteCountdown, secondCountdown)
     }
+    return '00:00:00';
+  });
 
-    const timer = setInterval(() => {
-        let dateTime;
-        dateTime = DateTime.now();
-        setCurrentDate(DateTime.now())
-    }, 1000);
+  const displayDayCountdown = createMemo<string>(() => {
+    const daysRemaning = Math.abs(DateTime.now().diff(payDate(), 'days').days);
+    return formatTimeNumber(daysRemaning);
 
-    const daysRemaning = Math.abs(DateTime.now().diff(props.paydayDate, 'days').days);
-    setDisplayDayCountdown(formatTimeNumber(daysRemaning));
+  });
 
-    let hourCountdown: number;
-    let minuteCountdown: number;
-    let secondCountdown: number;
+  const timer = setInterval(() => {
+    let dateTime;
+    dateTime = DateTime.now();
+    setCurrentDate(DateTime.now())
+  }, 1000);
 
-    createEffect(() => {
-        const durationDays = currentDate()?.diff(props.paydayDate, 'days').days;
-        if (durationDays) {
-            hourCountdown = (Math.abs(durationDays) % 1) * 24;
-            minuteCountdown = (hourCountdown % 1) * 60;
-            secondCountdown = (minuteCountdown % 1) * 60;
+  let hourCountdown: number;
+  let minuteCountdown: number;
+  let secondCountdown: number;
 
-            setCountdown(
-                formatTime(hourCountdown, minuteCountdown, secondCountdown)
-            );
-        }
-    });
+  onCleanup(() => clearInterval(timer));
 
-    onCleanup(() => clearInterval(timer));
-
-    return (
-        <div class="container mx-auto mb-32">
-            <p class="flex justify-end text-3xl relative -top-10">
-                {currentDate()?.toFormat('dd/MM/yyyy')} - 
-                {currentDate()?.toFormat('hh:mm:ss')}
-            </p>
-            <p class="flex justify-center text-5xl font-bold">
-                {displayDayCountdown()}:{countdown()}
-            </p>
-        </div>
-    );
+  return (
+    <div class="container mx-auto mb-32">
+      <p class="flex justify-end text-3xl relative -top-10">
+        {currentDate()?.toFormat('dd/MM/yyyy')} -
+        {currentDate()?.toFormat('hh:mm:ss')}
+      </p>
+      <p class="flex justify-center text-5xl font-bold">
+        {displayDayCountdown()}:{displayHoursCountdown()}
+      </p>
+    </div>
+  );
 };
